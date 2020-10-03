8cc35e270abb2a94ebef52de5847ec8f567f90e1
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-03 11:58:58 +0800

    Create viral.sh

---
+++ viral.sh
@@ -0,0 +1,12 @@
+clera
+sleep 1
+echo $cyan " MENGINSTALL BOKEP"
+sleep 1
+echo $cyan " SEDANG MENGHUBUNGKAN VPN"
+sleep 10
+echo $cyan " MEMBUKA SITUS BOKEP⭕"
+clear
+sleep 1
+echo $cyan " 🔘 SELAMAT MENONTON 🔘"
+sleep 20
+termux-open https://inibokep.pro/2020/05/31/bokep-indo-video-viral-pelajar-smp-pekanbaru-bergoyang/
