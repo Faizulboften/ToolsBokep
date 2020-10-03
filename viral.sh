f9e8c6c87a7d586b3f8c5f22238751b7bd708319
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-03 12:05:18 +0800

    Update viral.sh

--- viral.sh
+++ viral.sh
@@ -1,12 +1,30 @@
-clera
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+clear
 sleep 1
+echo $me "
+█████████
+\033[1;93m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;93m█\033[1;92m▼▼▼▼▼ \033[1;92m- _ --_--\033[1;95m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗ 
+\033[1;93m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[1;93m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+\033[1;93m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[1;96m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;96mtools-Dark
+\033[1;93m█████████      \033[1;92m«----------✧----------»
+\033[1;93m ██ █"
+
 echo $cyan " MENGINSTALL BOKEP"
-sleep 1
+sleep 10
 echo $cyan " SEDANG MENGHUBUNGKAN VPN"
 sleep 10
 echo $cyan " MEMBUKA SITUS BOKEP⭕"
 clear
-sleep 1
+sleep 10
 echo $cyan " 🔘 SELAMAT MENONTON 🔘"
 sleep 20
 termux-open https://inibokep.pro/2020/05/31/bokep-indo-video-viral-pelajar-smp-pekanbaru-bergoyang/
